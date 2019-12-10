6ceecead29e2b463a27e763817869b2331b737c7
Shiv C <shivakumargowda.chikkappa@infogroup.com>
2019-12-10 13:59:29 -0600

    Skeleton springboot application

---
+++ src/main/java/com/datahub/metadata/CustomerMetadataApplication.java
@@ -0,0 +1,13 @@
+package com.datahub.metadata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerMetadataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerMetadataApplication.class, args);
+	}
+
+}
